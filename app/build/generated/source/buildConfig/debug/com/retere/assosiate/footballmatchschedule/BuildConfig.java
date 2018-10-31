c4b21652454138957042f3e3280c1be49a27dece
Yana Andika <yanaandika@gmail.com>
2018-10-31 09:33:34 +0700

    versi 1.0

---
+++ app/build/generated/source/buildConfig/debug/com/retere/assosiate/footballmatchschedule/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.retere.assosiate.footballmatchschedule;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.retere.assosiate.footballmatchschedule";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Fields from default config.
+  public static final String BASE_URL = "https://www.thesportsdb.com/";
+  public static final String TSDB_API_KEY = "1";
+}
